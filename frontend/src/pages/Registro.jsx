e9bb379a12fb7b746e026fba8127cef215c10013
cjche005 <carlos.choquetarqui.et7@gmail.com>
2024-10-29 21:26:46 -0300

    Modificacion Registro frontend

--- frontend/src/pages/Registro.jsx
+++ frontend/src/pages/Registro.jsx
@@ -115,6 +115,8 @@ const Registro = () => {
                             <input type="text" className="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Antiguedad" />
                         </div>
 
+                        
+
                         <div className="relative flex items-center mt-6">
                             <span className="absolute">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-3 text-gray-300 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
