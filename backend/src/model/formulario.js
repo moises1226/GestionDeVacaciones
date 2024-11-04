998f41cb1c6e9d71c4451bbe89f8f7bc82f3ccf7
Moises <moises.aguilar.et7@gmailcom>
2024-11-04 07:04:25 -0300

    modificacion

---
+++ backend/src/model/formulario.js
@@ -0,0 +1,15 @@
+import sq from "../config/conexion-db.js"
+import { DataTypes } from "sequelize"
+
+const formulario = sq.define('formulario' , {
+
+    nombre : {
+
+        
+
+    }
+
+
+
+
+});
