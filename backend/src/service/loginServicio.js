31daaffa57ad277ab7f6c059f6839e40609da537
Moises <moises.aguilar.et7@gmail.com>
2024-11-11 12:56:07 +0000

    manejo de autenticacion x doble valor

--- backend/src/service/loginServicio.js
+++ backend/src/service/loginServicio.js
@@ -4,40 +4,53 @@ import administrador from '../model/administrador.js'
 
 // Servicio para autenticar usuario y administrador
 export const autenticarUsuarioServicio = async (gmail, contrasenia) => {
-
+   
+    // Primero, verificamos si el usuario existe en la base de datos
     const usuarioExistente = await usuario.findOne({ where: { gmail: gmail } });
     const adminExistente = await administrador.findOne({ where: { gmail: gmail } });
 
-    // Verificar si existe el usuario o el administrador
+    //verificacion de q si no exite en la db
     if (!usuarioExistente && !adminExistente) {
-        throw new Error("gmail no encontrado.");
+        throw new Error("El correo electrónico no está registrado.");
     }
 
-    // Verifica si es un usuario y compara la contraseña
+    // Verificación para el usuario
     if (usuarioExistente) {
         const contraseniaValidaUS = await bcrypt.compare(contrasenia, usuarioExistente.contrasenia);
-        if (!contraseniaValidaUS) {
-            throw new Error("La contraseña del usuario es incorrecta.");
+        if (contraseniaValidaUS) {
+            return {
+                permisos: usuarioExistente.permisos,
+            };
+        } else {
+            // Si la contraseña no es válida para el usuario, entonces verificamos si es un administrador
+            if (adminExistente) {
+                const contraseniaValidaAD = await bcrypt.compare(contrasenia, adminExistente.contrasenia);
+                if (contraseniaValidaAD) {
+                    return {
+                        permisos: adminExistente.permisos,
+                    };
+                } else {
+                    throw new Error("La contraseña del administrador es incorrecta.");
+                }
+            } else {
+                throw new Error("La contraseña del usuario es incorrecta.");
+            }
         }
-        return {
-            
-            permisos: usuarioExistente.permisos 
-        };  
     }
 
-    // Verifica si es un administrador y compara la contraseña
+    // Verificación para el administrador
     if (adminExistente) {
         const contraseniaValidaAD = await bcrypt.compare(contrasenia, adminExistente.contrasenia);
-        if (!contraseniaValidaAD) {
+        if (contraseniaValidaAD) {
+            return {
+                permisos: adminExistente.permisos,
+            };
+        } else {
             throw new Error("La contraseña del administrador es incorrecta.");
         }
-        return {
-            
-            permisos: adminExistente.permisos 
-        };  
-    
     }
 };
+
 
 // Servicio para manejar el logout (cerrar sesión)
 export const cerrarSesionServicio = async (req) => {
